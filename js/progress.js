65cfd894e300ca07dfc9d964a96dff0ae1b18474
An Cao <112416427+AnCao001@users.noreply.github.com>
2023-03-22 17:15:13 +0000

    Wed, Mar 22, 2023, 5:15 PM +00:00

---
+++ js/progress.js
@@ -0,0 +1,47 @@
+const progess = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+
+let currentActive = 1
+
+next.addEventListerner('click', () =>) {
+    currentActive++
+
+    if (curentActive > 4) {
+        currentActive = 4
+    }
+
+    update()
+}
+
+
+prev.addEventListener('click' , () =>{
+    currentAcitve--
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+functio update() {
+    progress.style.width = (currentActive - 1) /3 * 100 + '%'
+
+    if (currentActive == 1) {
+        pev.dishaload = true
+    }
+    else if (curentActive == 4)
+    `next.diabled
+
+    circles.forEach((circle, index) => {
+        if (index < currentAcitve {
+            circle.classList.add('active')
+        }
+        else {
+            circle.classList.remove('active')
+        }
+    })
+}
