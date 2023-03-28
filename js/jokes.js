5fccc3ccea790d9010e1580187283f2ee3fb978b
An Cao <112416427+AnCao001@users.noreply.github.com>
2023-03-28 17:10:46 +0000

    Tue, Mar 28, 2023, 5:10 PM +00:00

--- js/jokes.js
+++ js/jokes.js
@@ -3,7 +3,7 @@ const jokeBtn = document.getElementById('jokeBtn')
 
 generateJoke()
 
-jokeBtn.addEventListener('click' ,generateJoke)
+jokeBtn.addEventListener('click',generateJoke)
 
 async function generationJoke(){
     const res = await fetch('https://icanhazdadjoke.com' , {
