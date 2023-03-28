e0f1592c8452bdb5bcaa3fbf935f7d10be8137fc
An Cao <112416427+AnCao001@users.noreply.github.com>
2023-03-28 16:37:57 +0000

    Tue, Mar 28, 2023, 4:37 PM +00:00

--- js/loading.js
+++ js/loading.js
@@ -1,8 +1,19 @@
-const search = document.querySelector('.search')
-const btn = docment.querySelector('.btn')
-const input = document,querySelector('.input')
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
+const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
+
+
+let load = 0
+
+
+let int = setInterval(blurring, 30)
+
+function blurring(){
+    load++;
+    loadText.innerText = `${load}%`
+    if (load == 100) {
+        clearInterval(int)
+    }
+    bg.style.filter = `blur(${30 - 0.3*load}px)`
+    loadText.style.opacity = `${1-load/100}`
+}
