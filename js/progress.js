e81991aa20842d888bb5b425511b51267d51c256
An Cao <112416427+AnCao001@users.noreply.github.com>
2023-04-24 17:14:07 +0000

    Mon, Apr 24, 2023, 5:14 PM +00:00

--- js/progress.js
+++ js/progress.js
@@ -1,41 +1,47 @@
-const progess = document.getElementById('progress')
+const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
-console.log(progress)
+
+
 
 let currentActive = 1
 
-next.addEventListerner('click', () =>) {
+
+next.addEventListener('click', () => {
     currentActive++
 
-    if (curentActive > 4) {
+
+    if(currentActive > 4){
         currentActive = 4
     }
 
+
     update()
 })
 
 
-prev.addEventListener('click' , () => {
-    currentAcitve--
+prev.addEventListener('click', () => {
+    currentActive--
 
-    if (currentActive < 1) {
+
+    if (currentActive < 1){
         currentActive = 1
     }
-
     update()
 })
 
-function update() {
-    progress.style.width = (currentActive - 1) / 3 * 100 + '%'
 
-    if (currentActive == 1) {
+function update() {
+    progress.style.width = (currentActive - 1)/3 * 100 + '%'
+
+
+    if(currentActive == 1) {
         prev.disabled = true
     }
-    else if (curentActive == 4){
-        next.diabled = true
+    else if (currentActive == 4) {
+        next.disabled = true
     }
     else {
         prev.disabled = false
