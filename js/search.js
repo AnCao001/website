56e7c7c44034441f4bf401ab1039ed5eb1a814d1
An Cao <112416427+AnCao001@users.noreply.github.com>
2023-05-12 17:14:33 +0000

    Fri, May 12, 2023, 5:14 PM +00:00

--- js/search.js
+++ js/search.js
@@ -1,8 +1,8 @@
 const search = document.querySelector('.search')
 const btn = docment.querySelector('.btn')
-const input = document,querySelector('.input')
+const input = document.querySelector('.input')
 
-btn.addEventListener('click', () =? {
+btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
 })
