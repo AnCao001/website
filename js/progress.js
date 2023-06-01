dca1103a2e00da06f4ab071e9779518d3e29f238
An Cao <112416427+AnCao001@users.noreply.github.com>
2023-06-01 17:26:21 +0000

    Thu, Jun 1, 2023, 5:26 PM +00:00

--- js/progress.js
+++ js/progress.js
@@ -6,53 +6,51 @@ const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
-
 next.addEventListener('click', () => {
-    currentActive++
+   currentActive++
 
+   if (currentActive > 4) {
+       currentActive = 4
+   }
 
-    if(currentActive > 4){
-        currentActive = 4
-    }
-
-
-    update()
+   update()
 })
 
 
 prev.addEventListener('click', () => {
-    currentActive--
+   currentActive--
 
+   if (currentActive < 1) {
+       currentActive = 1
+   }
 
-    if (currentActive < 1){
-        currentActive = 1
-    }
-    update()
+   update()
 })
 
-
 function update() {
-    progress.style.width = (currentActive - 1)/3 * 100 + '%'
+   progress.style.width = (currentActive - 1) / 3 * 100 + '%'
 
 
-    if(currentActive == 1) {
-        prev.disabled = true
-    }
-    else if (currentActive == 4) {
-        next.disabled = true
-    }
-    else {
-        prev.disabled = false
-        next.disabled = false
-    }
+   if (currentActive == 1) {
+       prev.disabled = true
+   }
+   else if (currentActive == 4) {
+       next.disabled = true
+   }
+   else {
+       prev.disabled = false
+       next.disabled = false
+   }
 
 
-    circles.forEach((circle, index) => {
-        if (index < currentActive) {
-            circle.classList.add('active')
-        }
-        else {
-            circle.classList.remove('active')
-        }
-    })
+
+
+   circles.forEach((circle, index) => {
+       if (index < currentActive) {
+           circle.classList.add('active')
+       }
+       else {
+           circle.classList.remove('active')
+       }
+   })
 }
